c50d716f2cf18f8015115b055523ebcb48234d09
Sabitshafeexit(0clear <sabitshaafee@gmail.com>
2022-05-08 01:52:17 +0600

    newsletter part done

--- src/Pages/Home/Newsletter/Newsletter.js
+++ src/Pages/Home/Newsletter/Newsletter.js
@@ -3,15 +3,15 @@ import { Button, Form } from 'react-bootstrap';
 
 const Newsletter = () => {
     return (
-        <div>
-            <h1 className="text-center text-info">join our newsletter</h1>
+        <div style={{backgroundColor: 'beige'}}>
+            <h1 className="text-center text-info mt-5">join our newsletter</h1>
             <div className="container mx-auto w-50" >
                 <Form>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
+                        <Form.Label></Form.Label>
                         <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
+                        <Form.Text className="text-muted text-center">
+                        To get the latest news from us please subscribe your email
                         </Form.Text>
                     </Form.Group>
                 </Form>
